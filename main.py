df657d0869c9ece119d7e04a61a5f120e6a922b9
Dharshini R <23f1000851@ds.study.iitm.ac.in>
2026-07-08 15:45:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def predict():
             "variance": {},
             "min": {},
             "max": {},
-            "median": ["소득"],
+            "median": {"소득":45000},
             "mode": {},
             "range": {},
             "allowed_values": {},
